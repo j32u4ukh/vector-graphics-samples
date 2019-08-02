106a7d720190f27df756b77934359729234b3655
j32u4ukh <j32u4ukh@gmail.com>
2019-08-02 18:04:19 +0800

    一筆一筆劃出來，利用 Mesh

--- Assets/_Scripts/Spline1.cs
+++ Assets/_Scripts/Spline1.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Unity.VectorGraphics;
+using System.IO;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class Spline1 : MonoBehaviour
 {
     public Transform[] controlPoints;
@@ -13,6 +14,9 @@ public class Spline1 : MonoBehaviour
     private Shape m_Path;
     private VectorUtils.TessellationOptions m_Options;
     private Mesh m_Mesh;
+
+    Scene display_scene;
+    public float pen_width = 10f;
 
     void Start()
     {
@@ -27,12 +31,19 @@ public class Spline1 : MonoBehaviour
                 }
             },
             PathProps = new PathProperties() {
-                Stroke = new Stroke() { Color = Color.white, HalfThickness = 0.1f }
+                Stroke = new Stroke() {
+                    Color = Color.white,
+                    HalfThickness = 0.1f
+                }
             }
         };
 
         m_Scene = new Scene() {
-            Root = new SceneNode() { Shapes = new List<Shape> { m_Path } }
+            Root = new SceneNode() {
+                Shapes = new List<Shape> {
+                    //m_Path
+                }
+            }
         };
 
         m_Options = new VectorUtils.TessellationOptions() {
@@ -45,6 +56,104 @@ public class Spline1 : MonoBehaviour
         // Instantiate a new mesh, it will be filled with data in Update()
         m_Mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = m_Mesh;
+
+
+        // =======================================================================
+        string path = string.Format(@"D:\WriteByHand\svgs\{0}.svg", 20986);
+        string svg = SVGHelper.readSVG(path);
+        SVGParser.SceneInfo scene_info = SVGParser.ImportSVG(new StringReader(svg));
+        Scene scene = scene_info.Scene;
+        SceneNode word = scene.Root.Children[1];
+
+        // 前半為背景(無 Clipper)，後半為寫字筆劃(有 Clipper)
+        List<SceneNode> bg_and_stroke = word.Children;
+        int double_stroke_number = bg_and_stroke.Count;
+        int stroke_number = double_stroke_number / 2;
+
+        // 筆劃第一筆
+        SceneNode test_node = bg_and_stroke[stroke_number];
+        List<Shape> test_shapes = test_node.Shapes;
+        SceneNode test_clipper_node = (test_node.Clipper == null) ? null : test_node.Clipper;
+        List<Shape> test_clippers = new List<Shape>();
+        if (test_clipper_node != null)
+        {
+            test_clippers = test_clipper_node.Children[0].Shapes;
+            if (test_clippers != null)
+            {
+                print("test_clippers len:" + test_clippers.Count);
+                Shape test_clipper_shape = test_clippers[0];
+            }
+            else
+            {
+                print("test_clippers is null");
+            }
+        }
+        else
+        {
+            print("test_clipper_node is null");
+        }
+
+        Shape test_stroke = test_shapes[0];
+        BezierContour[] bezierContours = test_stroke.Contours;
+        BezierPathSegment[] bezierPathSegments = bezierContours[0].Segments;
+        BezierPathSegment point1 = bezierPathSegments[0];
+        BezierPathSegment point2 = bezierPathSegments[bezierPathSegments.Length - 1];
+
+        #region Word scene
+        // 遮罩嘗試
+        display_scene = new Scene()
+        {
+            Root = new SceneNode()
+            {
+                Children = new List<SceneNode>()
+                {
+                    #region One stroke
+                    new SceneNode()
+                    {
+                        Shapes = new List<Shape>()
+                        {
+                            #region Piece of stroke
+                            new Shape()
+                            {
+                                Contours = new BezierContour[]{
+                                    new BezierContour() {
+                                        Segments = new BezierPathSegment[]
+                                        {
+                                            point1,
+                                            point2
+                                        }
+                                    },
+                                    //new BezierContour() {
+                                    //    Segments = new BezierPathSegment[2]
+                                    //}
+                                },
+                                PathProps = new PathProperties() {
+                                    Stroke = new Stroke() {
+                                        Color = Color.white,
+                                        HalfThickness = 10f
+                                    }
+                                }
+                            }
+                            #endregion Piece of stroke end
+                        },
+                        Clipper = new SceneNode()
+                        {
+                            Shapes = new List<Shape>()
+                            {
+                                #region Piece of clipper
+                                test_clippers[0]
+                                #endregion Piece of clipper end
+                            }
+                        }
+
+                    }
+                    #endregion One stroke end
+                }
+            }
+        };
+        #endregion Word scene end
+
+        StartCoroutine(nextStroke(bg_and_stroke));
     }
 
     void Update()
@@ -55,20 +164,61 @@ public class Spline1 : MonoBehaviour
         }
 
         // Update the control points of the spline.
-        // line 1
-        m_Path.Contours[0].Segments[0].P0 = (Vector2)controlPoints[0].localPosition;
-        m_Path.Contours[0].Segments[0].P1 = (Vector2)controlPoints[1].localPosition;
-        m_Path.Contours[0].Segments[0].P2 = (Vector2)controlPoints[2].localPosition;
-        m_Path.Contours[0].Segments[1].P0 = (Vector2)controlPoints[3].localPosition;
+        //// line 1
+        //m_Path.Contours[0].Segments[0].P0 = (Vector2)controlPoints[0].localPosition;
+        //m_Path.Contours[0].Segments[0].P1 = (Vector2)controlPoints[1].localPosition;
+        //m_Path.Contours[0].Segments[0].P2 = (Vector2)controlPoints[2].localPosition;
+        //m_Path.Contours[0].Segments[1].P0 = (Vector2)controlPoints[3].localPosition;
 
-        // line 2
-        m_Path.Contours[1].Segments[0].P0 = (Vector2)controlPoints[4].localPosition;
-        m_Path.Contours[1].Segments[0].P1 = (Vector2)controlPoints[4].localPosition;
-        m_Path.Contours[1].Segments[0].P2 = (Vector2)controlPoints[4].localPosition;
-        m_Path.Contours[1].Segments[1].P0 = (Vector2)controlPoints[5].localPosition;
+        //// line 2
+        //m_Path.Contours[1].Segments[0].P0 = (Vector2)controlPoints[4].localPosition;
+        //m_Path.Contours[1].Segments[0].P1 = (Vector2)controlPoints[4].localPosition;
+        //m_Path.Contours[1].Segments[0].P2 = (Vector2)controlPoints[4].localPosition;
+        //m_Path.Contours[1].Segments[1].P0 = (Vector2)controlPoints[5].localPosition;
 
         // Tessellate the vector scene, and fill the mesh with the resulting geometry.
         var geoms = VectorUtils.TessellateScene(m_Scene, m_Options);
         VectorUtils.FillMesh(m_Mesh, geoms, 1.0f);
+    }
+
+    IEnumerator nextStroke(List<SceneNode> bg_and_stroke)
+    {
+        int stroke_number = bg_and_stroke.Count;
+        int half_number = stroke_number / 2;
+        int curr_stroke;
+        for(curr_stroke = 0; curr_stroke < half_number; curr_stroke++)
+        {
+            print("curr_stroke:" + curr_stroke);
+            //m_Scene.Root.Shapes.Add(bg_and_stroke[curr_stroke].Shapes[0]);
+        }
+        yield return new WaitForSeconds(Time.deltaTime);
+
+        for (; curr_stroke < stroke_number; curr_stroke++)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+            while (!Input.GetKeyDown(KeyCode.N))
+            {
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+            print("curr_stroke:" + curr_stroke);
+            
+            m_Scene.Root.Shapes.Add(newShape(bg_and_stroke[curr_stroke].Shapes[0].Contours));
+        }
+    }
+
+    Shape newShape(BezierContour[] contours)
+    {
+        return new Shape()
+        {
+            Contours = contours,
+            PathProps = new PathProperties()
+            {
+                Stroke = new Stroke()
+                {
+                    Color = Color.white,
+                    HalfThickness = 10f
+                }
+            }
+        };
     }
 }
--- Assets/_Scripts/Test.cs
+++ Assets/_Scripts/Test.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using Unity.VectorGraphics;
 using System.IO;
 
+//[ExecuteInEditMode]
 public class Test : MonoBehaviour
 {
     VectorUtils.TessellationOptions tessellation;
     List<VectorUtils.Geometry> geometries;
     SpriteRenderer render;
+
+    Scene scene;
+
+    Scene n_scene;
 
     private void Awake()
     {
@@ -29,7 +34,7 @@ public class Test : MonoBehaviour
         string path = string.Format(@"D:\WriteByHand\svgs\{0}.svg", 20986);
         string svg = SVGHelper.readSVG(path);
         SVGParser.SceneInfo scene_info = SVGParser.ImportSVG(new StringReader(svg));
-        Scene scene = scene_info.Scene;
+        scene = scene_info.Scene;
 
         // svg 本體
         SceneNode node = scene.Root;
@@ -45,62 +50,110 @@ public class Test : MonoBehaviour
         // 字的筆劃
         List<SceneNode> layer3 = node1.Children;
         //print("layer3:" + layer3.Count);
-        int n = 0;
-        foreach (SceneNode scene_node in layer3)
+        //int n = 0;
+        //foreach (SceneNode scene_node in layer3)
+        //{
+        //    print(string.Format("=== Node {0} ===", ++n));
+        //    List<Shape> shapes = scene_node.Shapes; // length = 1
+        //    Shape shape = shapes[0]; // length = 1
+        //    BezierContour[] contours = shape.Contours; // length = 1
+        //    BezierPathSegment[] segments = contours[0].Segments; // 每一筆劃的區段數量不同
+        //    int num = 0;
+        //    foreach(BezierPathSegment bezierPathSegment in segments)
+        //    {
+        //        print(string.Format("= segment {0} =", ++num));
+        //        print(bezierPathSegment.P0);
+        //        print(bezierPathSegment.P1);
+        //        print(bezierPathSegment.P2);
+        //        break;
+        //    }
+
+        //    SceneNode clipper = (scene_node.Clipper == null) ? null : scene_node.Clipper;
+        //    if(clipper != null)
+        //    {
+        //        List<SceneNode> clippers = clipper.Children; // length = 1
+        //        SceneNode mask = clippers[0];
+        //        List<Shape> m_shapes = mask.Shapes; // length = 1
+        //        Shape m_shape = m_shapes[0];
+        //        BezierContour[] m_contours = m_shape.Contours; // length = 1
+        //        //print("m_contours len:" + m_contours.Length);
+        //        BezierPathSegment[] m_segments = m_contours[0].Segments; // 每一筆劃的區段數量不同
+        //        //print("m_segments len:" + m_segments.Length);
+        //        int number = 0;
+        //        foreach (BezierPathSegment m_bezierPathSegment in m_segments)
+        //        {
+        //            print(string.Format("= clipper segment {0} =", ++number));
+        //            print(m_bezierPathSegment.P0);
+        //            print(m_bezierPathSegment.P1);
+        //            print(m_bezierPathSegment.P2);
+        //            break;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        print("clipper is null");
+        //    }
+        //}
+
+        n_scene = new Scene()
         {
-            print(string.Format("=== Node {0} ===", ++n));
-            List<Shape> shapes = scene_node.Shapes; // length = 1
-            Shape shape = shapes[0]; // length = 1
-            BezierContour[] contours = shape.Contours; // length = 1
-            BezierPathSegment[] segments = contours[0].Segments; // 每一筆劃的區段數量不同
-            int num = 0;
-            foreach(BezierPathSegment bezierPathSegment in segments)
+            Root = new SceneNode()
             {
-                print(string.Format("= segment {0} =", ++num));
-                print(bezierPathSegment.P0);
-                print(bezierPathSegment.P1);
-                print(bezierPathSegment.P2);
-                break;
-            }
-
-            //Matrix2D matrix2 = shape.FillTransform;
-            //PathProperties path_properties = shape.PathProps;
-            //Stroke stroke = path_properties.Stroke;
-
-            SceneNode clipper = (scene_node.Clipper == null) ? null : scene_node.Clipper;
-            if(clipper != null)
-            {
-                List<SceneNode> clippers = clipper.Children; // length = 1
-                SceneNode mask = clippers[0];
-                List<Shape> m_shapes = mask.Shapes; // length = 1
-                Shape m_shape = m_shapes[0];
-                BezierContour[] m_contours = m_shape.Contours; // length = 1
-                //print("m_contours len:" + m_contours.Length);
-                BezierPathSegment[] m_segments = m_contours[0].Segments; // 每一筆劃的區段數量不同
-                //print("m_segments len:" + m_segments.Length);
-                int number = 0;
-                foreach (BezierPathSegment m_bezierPathSegment in m_segments)
+                // node.Children  正常的字
+                // node1.Children  上下相反的字
+                //Children = node.Children
+                Children = new List<SceneNode>()
                 {
-                    print(string.Format("= clipper segment {0} =", ++number));
-                    print(m_bezierPathSegment.P0);
-                    print(m_bezierPathSegment.P1);
-                    print(m_bezierPathSegment.P2);
-                    break;
+                    new SceneNode()
+                    {
+                        Children = node1.Children
+                    }
                 }
             }
-            else
-            {
-                print("clipper is null");
-            }
-        }
+        };
 
-        sceneDisplay(scene, render);
+
+        //foreach (SceneNode sceneNode in layer3)
+        //{
+        //    n_scene.Root.Children.Add(sceneNode);
+        //}
+
+        sceneDisplay(n_scene, render);
+
+        //StartCoroutine(nextStroke(layer3));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+
+    IEnumerator nextStroke(List<SceneNode> layer)
+    {
+        int d_len = layer.Count;
+        int index = (int)(d_len / 2);
+        print(string.Format("d_len:{0}, index:{1}", d_len, index));
+
+        while(index < d_len)
+        {
+            n_scene.Root.Children.Add(layer[index]);
+            sceneDisplay(n_scene, render);
+            yield return new WaitForSeconds(Time.deltaTime);
+
+            while (!Input.GetKeyDown(KeyCode.N))
+            {
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+            
+            print("index:" + index);
+            print("n_scene.Root.Children:" + n_scene.Root.Children.Count);
+            index++;
+        }
+
+        print("End of stroke");
     }
 
     void sceneDisplay(Scene scene, SpriteRenderer render)
@@ -117,5 +170,55 @@ public class Test : MonoBehaviour
             true);
 
         render.sprite = sprite;
+    }
+
+    void wordScene()
+    {
+        #region Word scene
+        Scene display_scene = new Scene()
+        {
+            Root = new SceneNode()
+            {
+                Children = new List<SceneNode>()
+                {
+                    #region One stroke
+                    new SceneNode()
+                    {
+                        Shapes = new List<Shape>()
+                        {
+                            #region Piece of stroke
+                            // 每次產生一小段，塑造寫字的效果
+                            new Shape()
+                            {
+                                Contours = new BezierContour[]{
+                                    new BezierContour() {
+                                        Segments = new BezierPathSegment[2]
+                                    }
+                                }
+                            }
+                            #endregion Piece of stroke end
+                        },
+                        Clipper = new SceneNode()
+                        {
+                            Shapes = new List<Shape>()
+                            {
+                                #region Piece of clipper
+                                new Shape()
+                                {
+                                    Contours = new BezierContour[]{
+                                        new BezierContour() {
+                                            Segments = new BezierPathSegment[2]
+                                        }
+                                    }
+                                }
+                                #endregion Piece of clipper end
+                            }
+                        }
+                    }
+                    #endregion One stroke end
+                }
+            }
+        };
+        #endregion Word scene end
     }
 }
